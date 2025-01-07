15c440ff9395e79bc5e5b3c7eba0f22b3656fa16
Tikhonov-86 <Sashulka-86@list.ru>
2025-01-07 16:11:19 +0500

    Edit 07.01.25

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,11 @@
 # chat/urls.py
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.profile_view, name='profile'),
+    path('rooms/', views.room_choice_view, name='room_choice'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -10,5 +10,9 @@ def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
 
 
-def profile(request):
+def profile_view(request):
     return render(request, 'chat/profile.html')
+
+
+def room_choice_view(request):
+    return render(request, 'chat/room_choice.html')
